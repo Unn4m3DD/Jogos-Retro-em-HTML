ce5ab6a8d4b6c8cb1dea6609b0b96cb3fe4c1de3
AndrePatacas <guerreiroapatacas@gmail.com>
2019-09-18 16:25:21 +0100

    bugzito

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/tetris', (req, res) => res.sendFile(
 
     path.join(__dirname + '/Tetris/tetris_mark_2_aio.html'))
 )
-app.get('/', (req, res) => res.sendFile(
+app.get('/minesweeper', (req, res) => res.sendFile(
     path.join(__dirname + '/minesweeper_mark_1_aio.html')
 ))
 
